4fea96965668ab39f31d3acce94b754aa4920391
nodup309 <118347696+nodup309@users.noreply.github.com>
2023-05-02 22:36:18 +0200

    save

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,9 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Evenement;
+use App\Entity\Localisation;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +12,40 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        //admin
+        $admin = new User();
+        $admin->setUsername('admin')
+            ->setEmail('admin@test.com')
+            ->setPassword('password')
+            ->setRole('admin');
+        $manager->persist($admin);
+        //users
+        for($i = 0; $i < 5;$i++) {
+            $user = new User();
+            $user->setUsername('user')
+                ->setEmail('user@test.com')
+                ->setPassword('password')
+                ->setRole('user');
+            $manager->persist($user);
+        }
+
+        for($i = 0; $i < 5;$i++) {
+            $evenement = new Evenement();
+            $evenement->setName('test')
+                ->setDescription('description')
+                ->setDateDebut(new \DateTime())
+                ->setDateFin(new \DateTime())
+                ->setTypeEvenement('festival');
+            $manager->persist($evenement);
+
+            $localisation = new Localisation();
+            $localisation->setEvenement($evenement)
+                ->setRue('e')
+                ->setCP('e')
+                ->setVille('e');
+            $manager->persist($localisation);
+        }
+
 
         $manager->flush();
     }
